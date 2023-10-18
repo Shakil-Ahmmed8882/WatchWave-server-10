a54124af695d4b0f00c0de317a0f3cd436db94f8
Shakil-Ahmmed <Shakilahmmed8882@gmail.com>
2023-10-18 13:24:47 +0600

    Insrted Brands collection to the database

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 1000;
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 
 // middle ware
 app.use(cors());
 app.use(express.json());
 
-
 // =======================================
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sk8jxpx.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,16 +19,31 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("WatchWave");
+    const BrandsCollection = database.collection("BrandsCollection");
+
+    app.get("/", async (req, res) => {
+      const result = await BrandsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/addBrand", async (req, res) => {
+      const brand = req.body;
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -38,11 +52,6 @@ async function run() {
 run().catch(console.dir);
 
 //=======================================
-
-// Defining a GET route for the root URL ("/")
-app.get("/", async (req, res) => {
-  res.send("Server is Running");
-});
 
 // Start the server on the specified port.
 app.listen(port, () => {
